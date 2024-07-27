75871bb2a15bdb56043760b052e9f2375a4836de
suttonj07 <suttonj07@gmail.com>
2024-07-27 12:45:06 -0400

    sessions

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -10,11 +10,30 @@ router.post('/', async (req, res) => {
         where: { email: req.body.email }
     })
 
-    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+    if (!user) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
+    } else if (!await bcrypt.compare(req.body.password, user.passwordDigest)){
+        res.status(401).json({ message: `Password is incorrect for user` })
     } else {
+        req.session.userId = user.userId
+        console.log(req.session.userId)
         res.json({ user })
     }
 })
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId);
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
 
 module.exports = router
